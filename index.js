fa8a7058051cc7bf41b15221b48e2c1bac1e572a
Erick Castille <ecastille924@gmail.com>
2021-04-17 15:01:59 -0400

    lesson completed:

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your functions here
+function map (array, someFunction) {
+    let newArray = [];
+
+    for (let i = 0; i < array.length; i++)
+    {
+        newArray.push(someFunction(array[i]));
+    }
+
+    return newArray;
+}
+
+function reduce (array, someFunction, start) {
+    let tmp;
+
+    if(start) {
+        tmp = start;
+        for (let i = 0; i < array.length; i++) {
+            tmp = someFunction(array[i], tmp);
+        }
+    }
+
+    else {
+        tmp = array[0];
+        for (let i = 1; i < array.length; i++) {
+            tmp = someFunction(array[i], tmp);
+        }
+    }
+    return tmp;
+}
